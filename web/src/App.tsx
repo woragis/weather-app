44704bd6878c6454119913d7b58aa3a56e9fcb3e
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-09 02:51:16 -0300

    Initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+const getWeather = () => {};
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
