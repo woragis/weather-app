c9f6066e37cb7a2eb7c74037c6c80b8bef91cffc
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-28 22:33:12 -0300

    Mobile: integrate tailwindcss

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,tsx,ts,jsx}', './components/**/*.{js,tsx,ts,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
